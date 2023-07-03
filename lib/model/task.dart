b938d7208860a719c91e2d2e60a6001eb0d40b9a
CHRISANTHOS <uakuchie@gmail.com>
2023-07-03 16:48:18 +0100

    Create task model according to expected parameters

---
+++ lib/model/task.dart
@@ -0,0 +1,47 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class Task {
+  User? user = FirebaseAuth.instance.currentUser;
+
+  String? id;
+  String title;
+  String description;
+  String? location;
+  String time;
+  String image;
+  FieldValue? createdOn;
+
+  Task(
+      {this.id,
+      required this.title,
+      required this.description,
+      required this.time,
+      this.location,
+      required this.image,
+      this.createdOn});
+
+
+  factory Task.fromSnapshot(DocumentSnapshot<Object?> document) {
+    final data = (document.data()! as dynamic);
+
+    return Task(
+        id: document.id,
+        title: data['title'] ?? '',
+        description: data['description'] ?? '',
+        time: data['time'],
+        location: data['location'] ?? '',
+        image: data['taskImage_url'] ?? '');
+  }
+
+  Map<String, dynamic> toJson(){
+    return {
+      'title': title,
+      'description': description,
+      'time': time,
+      'location': location,
+      'taskImage_url': image,
+      'createdOn': createdOn
+    };
+  }
+}
